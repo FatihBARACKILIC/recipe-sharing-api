dfe67fcae9eb524377d01c786a1ba397c38beaad
FatihBARACKILIC <fatihb469@gmail.com>
2023-12-31 21:01:41 +0000

    Add Elysia framework and controllers, update server configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,25 @@
 import { PrismaClient } from "@prisma/client";
+import Elysia from "elysia";
+import { userController } from "./controllers/user.controller";
+import { recipeController } from "./controllers/recipe.controller";
+import { commentController } from "./controllers/comment.controller";
 
 const prisma = new PrismaClient();
+const app = new Elysia();
 
-export { prisma };
+app.use(userController as any);
+app.use(recipeController as any);
+app.use(commentController as any);
+app.all("/", () => {
+  console.log("Hello, World!");
+
+  return { message: "Hello, World!" };
+});
+
+const port = process.env.PORT ?? 4040;
+
+app.listen(port, () => {
+  console.log("Server is running on port 4040");
+});
+
+export { app, prisma };
