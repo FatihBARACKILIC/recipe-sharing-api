6cb831213ed099fa552fe3a6ba55cd53a9c656e3
FatihBARACKILIC <fatihb469@gmail.com>
2023-12-31 20:40:25 +0000

    Add comment controller for creating and retrieving comments

---
+++ src/controllers/comment.controller.ts
@@ -0,0 +1,52 @@
+import Elysia, { type Context } from "elysia";
+import {
+  createComment,
+  getAllCommentForRecipe,
+} from "../services/comment.service";
+import { verifyToken } from "../services/auth.service";
+
+const commentController = (app: Elysia) => {
+  app.post("/:recipeId/create-comment", async (context: Context) => {
+    try {
+      const authHeader = context.headers["authorization"];
+      const token = authHeader && authHeader.split(" ")[1];
+      const { recipeId = "0" } = context.params;
+
+      if (!token) throw new Error("Invalid Token");
+      else if (recipeId === "0") throw new Error("Invalid Recipe");
+
+      const verifiedToken = verifyToken(token);
+      const { body } = context.body as { body: string };
+
+      const newComment = await createComment({
+        body,
+        recipeId: +recipeId,
+        userId: verifiedToken?.id,
+      });
+
+      return newComment;
+    } catch (error: any) {
+      return {
+        error: error.message,
+      };
+    }
+  });
+
+  app.post("/:recipeId/comments", async (context: Context) => {
+    try {
+      const { recipeId = "0" } = context.params;
+
+      if (recipeId === "0") throw new Error("Invalid Recipe");
+
+      const comments = await getAllCommentForRecipe(+recipeId);
+
+      return { comments };
+    } catch (error: any) {
+      return {
+        error: error.message,
+      };
+    }
+  });
+};
+
+export { commentController };
